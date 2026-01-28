b5038a19a9f2693979dd450326efefa9d6dcaeb2
MuhammadHelga <128779298+MuhammadHelga@users.noreply.github.com>
2026-01-28 16:56:49 +0700

    Update ChatInput

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function ChatInput({ value, onChange, onSend }) {
   return (
-    <div className='bg-[#F9F9F9] px-4 py-3 flex gap-3 '>
+    <div className='bg-[#F9F9F9] px-4 py-3 flex gap-3'>
         <input
           type='text'
           value={value}
           onChange={(e) => onChange(e.target.value)}
           placeholder='Tulis Jawaban...'
-          className="flex-1 px-4 py-3 rounded-xl outline-none bg-white"
+          className="flex-1 px-4 py-3 rounded-xl bg-white"
         />
         <button 
           onClick={onSend}
