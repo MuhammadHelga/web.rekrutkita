27aa957a64e70095c21f730a87c637c0f64aac79
MuhammadHelga <128779298+MuhammadHelga@users.noreply.github.com>
2026-01-29 19:36:10 +0700

    Update ChatInterview

--- src/pages/ChatInterview.jsx
+++ src/pages/ChatInterview.jsx
@@ -158,7 +158,7 @@ function ChatInterview() {
             
             <ChatHeader isFinished={isFinished} />
 
-            <div className='flex-1 overflow-y-auto p-4 space-y-3 pt-24 pb-20'>
+            <div className='flex-1 overflow-y-auto p-4 space-y-3'>
                 {messages.map((msg) => (
                 <ChatBubble
                     key={msg.id}
