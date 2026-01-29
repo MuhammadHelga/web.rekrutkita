81d972864f804295913e223f993f9a01b5e437dc
MuhammadHelga <128779298+MuhammadHelga@users.noreply.github.com>
2026-01-29 19:08:41 +0700

    Update ChatInput

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -8,11 +8,11 @@ function ChatInput({ value, onChange, onSend }) {
           value={value}
           onChange={(e) => onChange(e.target.value)}
           placeholder='Tulis Jawaban...'
-          className="flex-1 px-4 py-3 rounded-xl bg-white border border-gray-300 outline-none"
+          className="flex-1 px-2 md:px-4 py-2 md:py-3 rounded-xl bg-white border border-gray-300 outline-none"
         />
         <button 
           onClick={onSend}
-          className='bg-[#FEBD5A] px-6 py-3 rounded-xl text-white font-semibold hover:bg-[#e6a845]'
+          className='bg-[#FEBD5A] px-3 md:px-6 py-2 md:py-3 rounded-xl text-white text-[16px] md:text-lg font-semibold hover:bg-[#e6a845]'
         >
           Kirim
         </button>
