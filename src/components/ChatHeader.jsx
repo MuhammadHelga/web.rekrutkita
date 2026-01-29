81de71ccd6feeabfdab4b65cd88d56285d35d3bf
MuhammadHelga <128779298+MuhammadHelga@users.noreply.github.com>
2026-01-29 19:15:31 +0700

    Create ChatHeader

---
+++ src/components/ChatHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ChatHeader({ isFinished }) {
+  return (
+    <div className='fixed top-0 left-0 w-full bg-[#0D3556] text-white px-6 py-4 text-lg font-semibold shadow-md z-[100]'>
+      {isFinished ? "Wawancara telah Selesai" : "Proses Wawancara Dimulai"}
+      <div className="mt-2">
+        <div className={`h-[3px] w-16 rounded-full transition-all duration-500
+          ${isFinished ? "bg-green-500" : "bg-yellow-400"}`}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ChatHeader;
