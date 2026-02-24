27e29ad431a7db98aa7a8799a54a2252c253a016
MuhammadHelga <128779298+MuhammadHelga@users.noreply.github.com>
2026-02-24 15:13:17 +0700

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import FormPages from "./pages/FormPages";
+import VerifyTokenPage from "./pages/VerifyTokenPage"
 import ChatInterview from './pages/ChatInterview'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <FormPages/> } />
+        <Route path="/verify-token" element={ <VerifyTokenPage/> } />
         <Route path="/wawancara" element={ <ChatInterview/> } />
       </Routes>
     </BrowserRouter>
